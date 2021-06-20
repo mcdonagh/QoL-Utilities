f859938b2f442c196ca5a1515042e03aa883ce56
shane mcdonagh <smcdonagh23@gmail.com>
2021-06-20 11:24:47 -0700

    setting up config screen
    working on getting the config screen working

--- Options.lua
+++ Options.lua
@@ -3,9 +3,18 @@ local addonName, RU = ...
 RU.OPT = {}
 local opt = RU.OPT
 opt.Panel = CreateFrame('Frame', 'Redbeard\'s Utilities', UIParent);
-opt.Panel.Name = 'Redbeard\'s Utilities'
+opt.Panel.name = 'Redbeard\'s Utilities'
 InterfaceOptions_AddCategory(opt.Panel);
 
 function opt.OpenConfig()
+	InterfaceOptionsFrame_Show()
 	InterfaceOptionsFrame_OpenToCategory(opt.Panel)
+end
+
+function opt.Okay()
+
+end
+
+function opt.Cancel()
+
 end
