b22f4adf7d8f03b0c0563f0b4e237807a0fa2106
shane mcdonagh <smcdonagh23@gmail.com>
2021-07-02 17:55:31 -0700

    mount journal scanning

--- Summon.lua
+++ Summon.lua
@@ -2,19 +2,44 @@ local addonName, QOLUtils = ...
 
 QOLUtils.SMN = {}
 local smn = QOLUtils.SMN
-smn.Favorites = {}
+local UsableMounts = {}
 
 hooksecurfunc(C_MountJournal, 'SetIsFavorite', smn.UpdateJournal)
 
+local GroundTypes = {}
+local FlyingTypes = {}
+local WaterTypes = {}
+local SpecialTypes = {}
+GroundTypes[230] = true   -- for most ground mounts
+GroundTypes[269] = true   -- for  [Reins of the Azure Water Strider] and  [Reins of the Crimson Water Strider]
+FlyingTypes[242] = true   -- for Swift Spectral Gryphon (hidden in the mount journal, used while dead in certain zones)
+FlyingTypes[247] = true   -- for  [Disc of the Red Flying Cloud]
+FlyingTypes[248] = true   -- for most flying mounts, including those that change capability based on riding skill
+WaterTypes[231] = true    -- for  [Riding Turtle] and  [Sea Turtle]
+WaterTypes[232] = true    -- for  [Vashj'ir Seahorse] (was named Abyssal Seahorse prior to Warlords of Draenor)
+WaterTypes[254] = true    -- for  [Reins of Poseidus],  [Brinedeep Bottom-Feeder] and  [Fathom Dweller]
+SpecialTyeps[241] = true  -- for Blue, Green, Red, and Yellow Qiraji Battle Tank (restricted to use inside Temple of Ahn'Qiraj)
+SpecialTyeps[284] = true  -- for  [Chauffeured Chopper] and Chauffeured Mechano-Hog
+
 function smn.ScanJournal()
+	UsableMounts = {}
 	for mountID = 1, C_MountJournal.GetMountIDs() do
-		local _, _, _, _, _, _, isFavorite = C_MountJournal.GetMountInfoByID(mountID)
-		smn.Favorites[mountID] = isFavorite
+		local _, _, _, _, _, _, isFavorite, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfoByID(mountID)
+		local _, _, _, _, typeID = C_MountJournal.GetMountInfoExtraByID(mountID)
+		if not hideOnChar and isCollected then
+			UsableMounts[mountID] = { isFavorite, typeID })
+		end
 	end
+	smn.Mount()
 end
 
 function smn.UpdateJournal(mountID, isFavorite)
-	smn.Favorites[mountID] = isFavorite
+	local _, _, _, _, typeID = C_MountJournal.GetMountInfoExtraByID(mountID)
+	if isFavorite then
+		UsableMounts[mountID] = { isFavorite, typeID }
+	else
+		UsableMounts[mountID] = nil
+	end
 end
 
 function smn.Summon(args)
@@ -37,7 +62,9 @@ function smn.Summon(args)
 end
 
 function smn.Mount()
-
+	for i, mountData in ipairs(UsuableMounts) do
+		smn.Log(format('i = %d | isFavorite | %s | typeID %s', i, mountData[1], mountData[2]) 
+	end
 end
 
 function smn.Pet()
